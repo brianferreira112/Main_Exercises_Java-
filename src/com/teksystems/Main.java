7a4d9e052188a230929200c0e9e762aa3e299cbc
BrianFerreira <17brianferreira@gmail.com>
2022-02-18 16:13:21 -0500

    Main Exercises

---
+++ src/com/teksystems/Main.java
@@ -0,0 +1,52 @@
+package com.teksystems;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*Write a program that declares 2 integer variables,
+        assigns an integer to each, and adds them together.
+        Assign the sum to a variable. Print out the result.
+         */
+        int x = 30;
+        int y = 20;
+        int z = x + y;
+        System.out.println(z);
+
+        /*Write a program that declares 2 double variables,
+        assigns a number to each, and adds them together.
+        Assign the sum to a variable.
+        Print out the result.
+         */
+        double x1 = 12.0;
+        double y1 = 8.0;
+        double z1 = x+y;
+        System.out.println(z);
+
+        /*Write a program that declares an integer variable and a double variable,
+        assigns numbers to each, and adds them together.
+        Assign the sum to a variable. Print out the result.
+        What variable type must the sum be?
+         */
+        int g = 2;
+        double f = 3;
+        double h = g+f;
+        System.out.println(h);
+
+        /*Write a program that declares 2 integer variables,
+        assigns an integer to each,
+        and divides the larger number by the smaller number.
+        Assign the result to a variable. Print out the result.
+        Now change the larger number to a decimal.
+        What happens? What corrections are needed?
+         */
+
+        int v=6;
+        int w=5;
+
+
+
+
+
+    }
+}
